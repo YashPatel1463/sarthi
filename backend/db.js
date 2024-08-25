25d008da96d68bfaa9bb2b6b3c1b26e3ba9ab138
Yash <patelyash140603@gmail.com>
2024-08-25 14:58:11 +0530

    second commit

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 function connectDB(){
 
-    // mongoose.connect(`mongodb+srv://nallapanenimahidhar2004:7YIIDryvdSdSISth@cluster0.oclfqi3.mongodb.net/mern?retryWrites=true&w=majority`, {useUnifiedTopology: true , useNewUrlParser: true})
-    mongoose.connect(`mongodb://localhost:27017/sarthi`, {useUnifiedTopology: true , useNewUrlParser: true})
+   mongoose.connect(`mongodb://localhost:27017/sarthi`, {useUnifiedTopology: true , useNewUrlParser: true})
 
     const connection = mongoose.connection
 
